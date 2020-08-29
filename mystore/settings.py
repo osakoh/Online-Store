6bd21ad219469564168cd03323c1affe5ebe3462
Michael Osarodion <macmive@gmail.com>
2020-08-29 17:44:10 +0100

    added static files and templates

--- mystore/settings.py
+++ mystore/settings.py
@@ -120,3 +120,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "mystore/../shop/static"),
+# ]
+
+# located outside the base directory to where the venv is
+STATIC_ROOT = os.path.join(BASE_DIR, "static_dev")
+
+# LOGIN_REDIRECT_URL = 'post_list'
+
+# LOGOUT_REDIRECT_URL = '/login/'
+
+# LOGIN_URL = '/login/'
+
+# media settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # creates a 'media' folder in the root dir (Housing) directory
+MEDIA_URL = '/media/'
--- mystore/urls.py
+++ mystore/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls'), namespace='shop'),  # namespace must be the same as app_name
+    path('', include('shop.urls', namespace='shop')),  # namespace must be the same as app_name
 ]
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # required for media files(pictures) to show on the website
