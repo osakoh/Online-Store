eeb4adae2f34099926997d2eceeeb420022d1cff
Michael Osarodion <macmive@gmail.com>
2020-08-29 15:45:51 +0100

    added CategoryAdmin&ProductAdmin in shop admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import (Category, Product)
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}  # automatically pre-populates the slug field using the category name
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']  # fields to be shown on the admin page
+    list_filter = ['available', 'created', 'updated']  # shows a filter section on the right side of admin page
+    list_editable = ['price', 'available']  # makes fields editable(changeable)
+    prepopulated_fields = {'slug': ('name',)}  # automatically pre-populates the slug field using the product name
