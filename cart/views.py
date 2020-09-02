f71ed02d45117b3beeaa39c4487ccea23aabd074
Michael Osarodion <macmive@gmail.com>
2020-09-02 14:22:44 +0100

    updated the cart_detail view so as to modify an order in the cart

--- cart/views.py
+++ cart/views.py
@@ -46,6 +46,14 @@ def cart_remove(request, product_id):
 
 def cart_detail(request):
     cart = Cart(request)
+
+    # the form is initialised with the current item quantity and the
+    # override field is set to True so when the form is submitted to 'cart_add' view, the current quantity
+    # is replace with a new one.
+    for item in cart:
+        item['update_quantity'] = CartAddProductForm(initial={'quantity': item['quantity'],
+                                                              'override': True})
+
     context = {'cart': cart}
     return render(request, 'cart/detail.html', context)
 
