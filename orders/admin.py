99f3a216664222eefe30f45d93a7c0b4bda20571
Michael Osarodion <macmive@gmail.com>
2020-09-03 12:31:20 +0100

    added Order&OrderItems model to admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    # this id_field must be a foreign key or a many-to-many field
+    raw_id_fields = ['product']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    # sets fields to be displayed on the admin page
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code',
+                    'city', 'paid', 'created', 'updated']
+    list_filter = ['paid', 'created', 'updated']  # filter section on the right side of admin page
+    list_per_page = 15  # specify max amount of items to be displayed on the admin page
+    inlines = [OrderItemInline]
