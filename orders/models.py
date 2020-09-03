7aadb184d28f7ba3736375f968f0ade83b0ff169
Michael Osarodion <macmive@gmail.com>
2020-09-03 12:10:36 +0100

    OrderItem model added in orders app

--- orders/models.py
+++ orders/models.py
@@ -3,6 +3,10 @@ from shop.models import Product
 
 
 class Order(models.Model):
+    """
+    this model stores customers information with a 'paid-BooleanField' which defaults to False.
+    get_total_cost() is to calculate the total cost of items bought in a particular order.
+    """
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email = models.EmailField()
@@ -14,14 +18,30 @@ class Order(models.Model):
     paid = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ('-created')
+        ordering = ('-created',)
 
     def __str__(self):
         return "Order {}".format(self.id)
 
     def get_total_cost(self):
-        return sum(item.get_cost() for item in self.item.all())
+        return sum(item.get_cost() for item in self.items.all())  # items here points to the 'related_name' in OrderItem
 
 
+class OrderItem(models.Model):
+    """
+    this model stores the product, quantity, and price for each item.
+    get_cost() returns the cost of items in an order.
+    """
+    # - an OrderItem belongs to one person(Order) and one person(Order)
+    # can have multiple OrderItems (one-to-many relationship)
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)  # an integer field but must be 1 or 0; can't be negative
 
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
 
